4b81d070cb228b7dcd115ffa4d8afd5f40829c21
senya_1997 <senya_1997@mail.ru>
2020-05-13 00:55:02 +0300

    cut the FHT from repo

--- matlab/fft_but.m
+++ matlab/fft_but.m
@@ -1,4 +1,4 @@
-function fft_but4 = fft_but4(x, N, n) % length 'x' = 4: DFT-4
+function fft_but = fft_but(x, N, n) % length 'x' = 4: DFT-4
 
 but_re(1) = (real(x(1)) + real(x(2)) + real(x(3)) + real(x(4)))/4;
 but_re(2) = (real(x(1)) + imag(x(2)) - real(x(3)) - imag(x(4)))/4;
@@ -15,12 +15,12 @@ w(2) = cos(1*n*(2*pi/N)) - 1i*sin(1*n*(2*pi/N));
 w(3) = cos(2*n*(2*pi/N)) - 1i*sin(2*n*(2*pi/N));
 w(4) = cos(3*n*(2*pi/N)) - 1i*sin(3*n*(2*pi/N));
 
-fft_but4(1) = but_re(1) + 1i*but_im(1);
-fft_but4(2) = but_re(2) + 1i*but_im(2);
-fft_but4(3) = but_re(3) + 1i*but_im(3);
-fft_but4(4) = but_re(4) + 1i*but_im(4);
+fft_but(1) = but_re(1) + 1i*but_im(1);
+fft_but(2) = but_re(2) + 1i*but_im(2);
+fft_but(3) = but_re(3) + 1i*but_im(3);
+fft_but(4) = but_re(4) + 1i*but_im(4);
 
-fft_but4(1) = fft_but4(1)*w(1);
-fft_but4(2) = fft_but4(2)*w(2);
-fft_but4(3) = fft_but4(3)*w(3);
-fft_but4(4) = fft_but4(4)*w(4);
+fft_but(1) = fft_but(1)*w(1);
+fft_but(2) = fft_but(2)*w(2);
+fft_but(3) = fft_but(3)*w(3);
+fft_but(4) = fft_but(4)*w(4);
